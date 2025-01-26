438734bef051075c4d69fe121cd8344a0ee93e2b
Horrismaina <gacheruhorris@gmail.com>
2025-01-26 19:50:47 +0300

    intro to classes

---
+++ oop/library_system.py
@@ -0,0 +1,46 @@
+class Book:
+    def __init__(self, title, author):
+        """Base Book class constructor"""
+        self.title = title
+        self.author = author
+
+    def __str__(self):
+        """String representation of the book"""
+        return f"Book: {self.title} by {self.author}"
+
+
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        """EBook constructor inheriting from Book"""
+        super().__init__(title, author)
+        self.file_size = file_size
+
+    def __str__(self):
+        """String representation of the EBook"""
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+
+
+class PrintBook(Book):
+    def __init__(self, title, author, page_count):
+        """PrintBook constructor inheriting from Book"""
+        super().__init__(title, author)
+        self.page_count = page_count
+
+    def __str__(self):
+        """String representation of the PrintBook"""
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+
+
+class Library:
+    def __init__(self):
+        """Library constructor initializing book collection"""
+        self.books = []
+
+    def add_book(self, book):
+        """Add a book to the library collection"""
+        self.books.append(book)
+
+    def list_books(self):
+        """Print details of all books in the library"""
+        for book in self.books:
+            print(book)
---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,28 @@
+import math
+
+class Shape:
+    def area(self):
+        """Base method to be overridden by subclasses"""
+        raise NotImplementedError("Subclass must implement abstract method")
+
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        """Rectangle constructor"""
+        self.length = length
+        self.width = width
+
+    def area(self):
+        """Calculate rectangle area"""
+        return self.length * self.width
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        """Circle constructor"""
+        self.radius = radius
+
+    def area(self):
+        """Calculate circle area"""
+        return math.pi * self.radius ** 2
+    
